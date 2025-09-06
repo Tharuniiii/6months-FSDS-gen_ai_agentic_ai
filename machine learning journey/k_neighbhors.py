0596968c959baa11f5ecbdc0b56c2bfd31b74614
tharuni teegala <107245325+Tharuniiii@users.noreply.github.com>
2025-09-06 22:40:06 +0530

    Add files via upload

---
+++ machine learning journey/k_neighbhors.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(r"C:\Users\Tharuni\Desktop\NIT\sept month\3rd,4th_confusion matrix practicle\data\Social_Network_Ads.csv")
+
+x = dataset.iloc[:,[2,3]].values
+y = dataset.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.20,random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=4,p=1)
+classifier.fit(x_train,y_train)
+y_pred = classifier.predict(x_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# This is to get the Models Accuracy 
+from sklearn.metrics import accuracy_score 
+ac = accuracy_score(y_test, y_pred)
+print(ac) 
+
+
+# This is to get the Classification Report
+from sklearn.metrics import classification_report
+cr = classification_report(y_test, y_pred)
+cr
+
+bias = classifier.score(x_train,y_train)
+bias
+
+variance = classifier.score(x_test,y_test)
+variance
+
+from matplotlib.colors import ListedColormap
+x_set,y_set = x_train,y_train
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+
+
+
+
+
+
+
