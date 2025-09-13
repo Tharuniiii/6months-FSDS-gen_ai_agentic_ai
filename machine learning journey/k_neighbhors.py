f6a3e4bdf639c207d761d46b955346084137df57
tharuni teegala <107245325+Tharuniiii@users.noreply.github.com>
2025-09-13 10:55:01 +0530

    Add files via upload

--- machine learning journey/k_neighbhors.py
+++ machine learning journey/k_neighbhors.py
@@ -41,12 +41,43 @@ bias
 variance = classifier.score(x_test,y_test)
 variance
 
+#training set results
 from matplotlib.colors import ListedColormap
 x_set,y_set = x_train,y_train
 x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(x1.min(),x1.max())
+plt.ylim(x1.min(),x1.max())
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set==j,0],x_set[y_set==j,1],
+                c =ListedColormap(('red','green'))(i),label=j)
+plt.title('KNN-Training set')
+plt.xlabel('Age')
+plt.ylabel('Estimated salary')
+plt.legend()
+plt.show()
+
+# test set results
+from matplotlib.colors import ListedColormap
+x_set,y_set = x_test,y_test
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(x1, x2, classifier.predict(np.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN test results')
+plt.xlabel('Age')
+plt.ylabel('Estimated salary')
+plt.legend()
+plt.show()
+
+
 
 
 
